849081191571bea4350f5c0793709175ad078db3
Guido D'Orsi <gu.dorsi@gmail.com>
2022-10-12 17:43:11 +0200

    copy the header from solid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -58,22 +58,40 @@ export default component$(() => {
 
   return (
     <main>
-      <h1>
-        Wikipedia search:{" "}
+      <nav class="border fixed split-nav navbar">
+        <div class="nav-brand">
+          <a href="https://romajs.org/">RomaJS</a>
+        </div>
         <form
+          class="form-group query-form"
           preventDefault:submit
           onSubmit$={(event) => {
             const { elements } = event.target as any;
 
-            searchTerm.value = elements.search.value;
+            searchTerm.value = elements.q.value;
           }}
         >
-          <input name="search" value={initialValue} disabled={loading} />
-          <button type="submit" disabled={loading}>
-            Ok
+          <label class="visually-hidden" for="q">
+            Search wikipedia
+          </label>
+          <input
+            type="search"
+            name="q"
+            placeholder="query wikipedia"
+            id="q"
+            value={initialValue}
+            disabled={loading}
+          />
+          <button type="submit" class="btn-small" disabled={loading}>
+            <span class="visually-hidden">submit</span>
+            <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
+              <g>
+                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+              </g>
+            </svg>
           </button>
         </form>
-      </h1>
+      </nav>
       <Resource
         value={searchResource}
         onPending={() => <div>Loading...</div>}
